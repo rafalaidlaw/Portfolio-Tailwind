a012423248b15446598e0ac20d0c340c1a28359e
rafalaidlaw <rafalaidlaw@gmail.com>
2024-11-28 10:03:50 -0500

    Edited all outstanding SVG code to be compatible with Tailwind.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,10 +7,10 @@ const Skills = () => {
     <section className="py-5 align-element bg-orange-400" id="skills">
       <SectionTitle
         text="React.js Redux Typescript Node.js Firebase C# SASS Tailwind
-             Angular JavaScript ActionScript Git Markdown 
+             Angular JavaScript JQuery ActionScript Git Markdown 
             BootStrap Flexbox/CSS HTML GraphQL JSON XML MUI"
       />
-      <div className="py-7 gap-8 flex flex-wrap">
+      <div className="py-5 gap-3 md:gap-5 flex flex-wrap justify-center">
         {skills.map((skill) => {
           return <SkillsCard key={skill.id} {...skill} />;
         })}
