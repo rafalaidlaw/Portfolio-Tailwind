ea05209ca68085f759e274ea0aef4908c170b50e
Rafael Laidlaw <rafalaidlaw@gmail.com>
2025-03-03 18:16:28 -0500

    Added footer component. Made absolute center placeholder image for future use. Touched up some layout issues.

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,40 @@ import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import Footer from "./components/Footer";
+import nubbin from "./assets/nubbinsvg.svg";
 
 const App = () => {
   return (
     <>
-      <div className="bg-orange-900">
-        <div className="md:sticky top-0 z-50">
+      <div className="bg-orange-900 ">
+        <div className="md:sticky top-0 z-50  ">
           <Navbar />
+          <article className=" grid grid-flow-col grid-rows-1">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div className="">
+              <img
+                src={nubbin}
+                alt="Packaging Shadow"
+                className="h-50 flex absolute"
+              />
+            </div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </article>
         </div>
         <div className="border-orange-600 border-r-8 border-l-8">
           <div className="border-l-8 border-orange-700">
@@ -17,15 +44,21 @@ const App = () => {
               <div className="border-r-8 border-orange-700">
                 <div className="border-r-8 border-orange-700">
                   <div className=" border-l-8 border-orange-800">
-                    <div className=" border-8 border-orange-800">
-                      <div className=" p-14">
-                        <div>
-                          <Hero className="bg-orange-400 py-5 px-4" />
-                        </div>
+                    <div className=" border-r-8 border-orange-800">
+                      <div className=" border-r-8 border-orange-800">
+                        <div className=" border-l-8 border-orange-800">
+                          <div className=" border-t-8 border-orange-800">
+                            <div className=" p-14">
+                              <div>
+                                <Hero className="bg-orange-400 py-5 px-4" />
+                              </div>
 
-                        <Skills />
-                        <About />
-                        <Projects />
+                              <Skills />
+                              <About />
+                              <Projects />
+                            </div>
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -35,6 +68,7 @@ const App = () => {
           </div>
         </div>
       </div>
+      <Footer />
     </>
   );
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,51 @@
+const Footer = () => {
+  return (
+    <div className="bg-orange-700">
+      <div className="border-b-2 border-orange-900 ">
+        <div className="border-l-2 border-orange-600 ">
+          <div className="border-r-2 border-orange-600">
+            <div className="border-b-2 border-orange-900 ">
+              <div className="border-l-2 border-orange-600 ">
+                <div className="border-r-2 border-orange-600">
+                  <div className="border-b-2 border-orange-900 ">
+                    <div className="border-l-2 border-orange-600 ">
+                      <div className="border-r-2 border-orange-600">
+                        <div className="border-b-2 border-orange-900 ">
+                          <div className="border-l-2 border-orange-600 ">
+                            <div className="border-r-2 border-orange-600">
+                              <div className="border-b-2 border-orange-900 ">
+                                <div className="border-l-2 border-orange-600 ">
+                                  <div className="border-r-2 border-orange-600">
+                                    <div className="border-b-8 border-orange-700 "></div>
+                                    <div className="border-b-8 border-orange-700 "></div>
+
+                                    <nav className="bg-orange-700  ">
+                                      <div className=" align-element flex flex-col sm:flex-row sm:gap-x-16 sm:items-center animate-fade1">
+                                        <article className="grid grid-rows-2">
+                                          <p className="lg:text-center sm:text-left relative font-mono text-orange-500">
+                                            You have reached the end of the
+                                            page.
+                                          </p>
+                                        </article>
+                                      </div>
+                                    </nav>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
