5229d25e44f45e9e5f26d7d370c1ee3f73ce59f6
Rafael Laidlaw <rafalaidlaw@gmail.com>
2025-03-16 09:38:21 -0400

    Scale projects to 110. Changed the links in Navbar.

--- src/data.jsx
+++ src/data.jsx
@@ -16,8 +16,8 @@ import heroImg from "./assets/Rafa_Bale_Developer_Reel.gif";
 export const links = [
   { id: nanoid(), href: "#home", text: "home" },
   { id: nanoid(), href: "#skills", text: "skills" },
-  { id: nanoid(), href: "#about", text: "about" },
   { id: nanoid(), href: "#projects", text: "portfolio" },
+  { id: nanoid(), href: "#about", text: "about" },
 ];
 
 export const skills = [
